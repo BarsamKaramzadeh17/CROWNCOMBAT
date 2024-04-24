23f0d295ffc4d72e8322a2b669cf97142fd3ed9d
Arian <arianabdollahi83@gmail.com>
2024-04-24 18:41:57 +0330

    test

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,35 @@
+import javax.swing.*;
+import javax.swing.Timer;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Main {
+    Timer timer;
+    int second;
+
     public static void main(String[] args){
-        int f=1;
-        System.out.println(f);
+        new Main();
     }
+    public Main(){
+        second=0;
+        while (true){
+            sampleTimer();
+            timer.start();
+            if(second%2==0){
+                System.out.println("Now time is Even");
+            }
+            else {
+                System.out.println("Now is odd");
+            }
+        }
+
+    }
+    public void sampleTimer(){
+        timer = new Timer(1000 , new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                second++;
+            }
+        })
+    ;};
 }
